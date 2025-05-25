18c6d907ca17fdcb1fb3bc2816267fec022e4768
Gunel04 <alkhasovagunel@gmail.com>
2025-05-25 19:24:22 +0400

    rostay-final-project

---
+++ Rostay Restaurant/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='container-fluid'>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
