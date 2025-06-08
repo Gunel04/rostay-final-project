f8f97444c396445c8994201103c20a1ad18b8acd
Gunel04 <alkhasovagunel@gmail.com>
2025-06-08 23:42:07 +0400

    rostay-restaurant-project

--- Rostay Restaurant/src/components/home/Experience.jsx
+++ Rostay Restaurant/src/components/home/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
         <section className="experience-section container-fluid">
             <div className="experience-text">
                 <h5>Experience</h5>
-                <h1>Exlusive Rostay Experiences</h1>
+                <h1>Exclusive Rostay Experiences</h1>
             </div>
             <div className="row">
                 <div className="col">
@@ -15,7 +15,7 @@ const Experience = () => {
                         </div>
                         <div className="experience-card-info">
                             <h1>Ambience</h1>
-                            <h2>Unique Atmosphere</h2>
+                            <h3>Unique Atmosphere</h3>
                             <p>Rostay Restaurant offers the perfect combination of sublime food, seamless service and an atmosphere of casual elegance</p>
                         </div>
                     </div>
@@ -23,40 +23,27 @@ const Experience = () => {
                 <div className="col">
                     <div className="experience-cards">
                         <div className="experience-card-img">
-                            <img src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/H3-exclusive-1.jpg" alt="" />
+                            <img className='center-img' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/H3-exclusive-2.jpg" alt="" />
                         </div>
                         <div className="experience-card-info">
-                            <h1>Ambience</h1>
-                            <h2>Unique Atmosphere</h2>
-                            <p>Rostay Restaurant offers the perfect combination of sublime food, seamless service and an atmosphere of casual elegance</p>
+                            <h1>Food & Wines</h1>
+                            <h3>Selected Quality</h3>
+                            <p>We offer a carefully prepared selection of culinary delights in favor of exquisite freshness and the highest standards.</p>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="experience-cards">
                         <div className="experience-card-img">
-                            <img src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/H3-exclusive-1.jpg" alt="" />
+                            <img src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/H3-exclusive-3.jpg" alt="" />
                         </div>
                         <div className="experience-card-info">
-                            <h1>Ambience</h1>
-                            <h2>Unique Atmosphere</h2>
+                            <h1>The Cooking</h1>
+                            <h3>The Chef Secrets</h3>
                             <p>Rostay Restaurant offers the perfect combination of sublime food, seamless service and an atmosphere of casual elegance</p>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="experience-cards">
-                        <div className="experience-card-img">
-                            <img src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/H3-exclusive-1.jpg" alt="" />
-                        </div>
-                        <div className="experience-card-info">
-                            <h1>Ambience</h1>
-                            <h2>Unique Atmosphere</h2>
-                            <p>Rostay Restaurant offers the perfect combination of sublime food, seamless service and an atmosphere of casual elegance</p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </section>
     )
