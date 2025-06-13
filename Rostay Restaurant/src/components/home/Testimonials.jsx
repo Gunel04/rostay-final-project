7f651a92a7185a792d8ec999891dd76939bb1022
Gunel04 <alkhasovagunel@gmail.com>
2025-06-13 15:50:53 +0400

    rostay-restaurant-project

--- Rostay Restaurant/src/components/home/Testimonials.jsx
+++ Rostay Restaurant/src/components/home/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa';
 import Slider from 'react-slick';
 
 const Testimonials = () => {
@@ -7,6 +8,7 @@ const Testimonials = () => {
         arrows: false,
         infinite: true,
         speed: 500,
+        autoplay:true,
         slidesToShow: 3,
         slidesToScroll: 1,
         initialSlide: 0,
@@ -18,7 +20,16 @@ const Testimonials = () => {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 880,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -26,8 +37,8 @@ const Testimonials = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     initialSlide: 2
                 }
             },
@@ -41,37 +52,53 @@ const Testimonials = () => {
         ]
     };
     return (
-        <div className="testimonial-section">
+        <div className="testimonial-section container-fluid">
             <div className="testimonial-info">
-                <h5>Our Happy Clients</h5>
+                <h6>Our Happy Clients</h6>
                 <h1>Customers Testimonials</h1>
             </div>
             <div className="testimonial-slider">
                 <div className="slider-container">
                     <Slider {...settings}>
                         <div className='slide-item'>
-                            <h3>1</h3>
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <p>“ With a capacity for 28 seated guests this option will suit larger gatherings. Smaller parties will benefit from the additional space area is required, available in combination with coffees and cold drinks to keep the team fueled.”</p>
+                            <h5>Addie Mckinney</h5>
+                            <h6>Pastry Chef</h6>
                         </div>
                         <div className='slide-item'>
-                            <h3>2</h3>
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <p>“ We carry Wellsford Cabinetry among several others for our kitchen design and remodel services. Wellsford has the best quality I have seen, excellent customer service. They are my “go to” line. ”</p>
+                            <h5>Alfred Mason</h5>
+                            <h6>Ceo & Founder</h6>
                         </div>
                         <div className='slide-item'>
-                            <h3>3</h3>
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <p>“I ate a taco from there and it came with pineapples and that was the first time I ever had pineapples with my el pastor tacos and it works really well and was delicious and made very rapidly per my request”</p>
+                            <h5>Sharon Gunther</h5>
+                            <h6>Fresh Design</h6>
                         </div>
                         <div className='slide-item'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='slide-item'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='slide-item'>
-                            <h3>6</h3>
-                        </div>
-                        <div className='slide-item'>
-                            <h3>7</h3>
-                        </div>
-                        <div className='slide-item'>
-                            <h3>8</h3>
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <FaStar color='#f2b612' />
+                            <p>“ Perfect for all kinds of meetings including business sessions, strategy days and team bonding activities for up to 62. Available in with or with pastries, coffees and cold drinks to keep the team fueled.”</p>
+                            <h5>Jordan Harper</h5>
+                            <h6>Master Chef</h6>
                         </div>
                     </Slider>
                 </div>
