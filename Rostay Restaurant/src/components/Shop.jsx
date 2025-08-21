176374123643c08da68f69f4327f7c2b7c1f1913
Gunel04 <alkhasovagunel@gmail.com>
2025-08-21 14:35:45 +0400

    rostay-sorting-pagination

--- Rostay Restaurant/src/components/Shop.jsx
+++ Rostay Restaurant/src/components/Shop.jsx
@@ -16,7 +16,12 @@ const Shop = () => {
   const [active, setActive] = useState("");
 
   const categories = useSelector(p => p.category);
-  const products = useSelector(p => p.product);
+  const [products, setProducts] = useState(useSelector(p => p.product));
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(6);
+
+
 
   const { addItem, inCart, emptyCart } = useCart();
   const { addWishlistItem, inWishlist, removeWishlistItem } = useWishlist();
@@ -25,6 +30,35 @@ const Shop = () => {
     const filteringProcess = products.filter(p => p.category === category);
     setFilteredData(filteringProcess);
     setActive(category);
+  }
+
+  const sortProducts = (sortingType) => {
+    const sortedProducts = [...products];
+    if (sortingType === 'price-asc') {
+      sortedProducts.sort((a, b) => a.price - b.price);
+    }
+    else if (sortingType === 'price-desc') {
+      sortedProducts.sort((a, b) => b.price - a.price);
+    }
+    else if (sortingType === 'name-asc') {
+      sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    else if (sortingType === 'name-desc') {
+      sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
+    }
+    setProducts(sortedProducts);
+
+  }
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+  console.log(currentProducts);
+
+  const pageNumbers = [];
+  
+  for(let i=1; i<=Math.ceil(products.length / productsPerPage); i++) {
+    pageNumbers.push(i);
   }
 
   return (
@@ -40,7 +74,7 @@ const Shop = () => {
               <h4>Product Categories</h4>
               <ul>
                 {categories.map((item, index) => (
-                  <li key={index} className={`${active === item.categoryName ? "active-category" : ""}`} style={{ cursor: "pointer" }} onClick={() => { filterProducts(item.categoryName) }}>{item.categoryName}</li>
+                  <li key={index} className={`${active === item.categoryName ? "active-category" : ""}`} style={{ cursor: "pointer" }} onClick={() => { filterProducts(item.categoryName) }}>{item.categoryName} ({products.filter(p=>p.category === item.categoryName).length})</li>
                 ))}
                 <li style={{ color: "#f2b612", cursor: "pointer" }} onClick={() => { filterProducts() }}>All Products</li>
               </ul>
@@ -53,7 +87,7 @@ const Shop = () => {
             </div>
           </div>
           <div className="right-products-part" >
-            <select name="" id="" className='sorting-selection'>
+            <select name="" id="" className='sorting-selection' onChange={e => sortProducts(e.target.value)}>
               <option value="sort-by">Sort By</option>
               <option value="name-asc">Name: A to Z</option>
               <option value="name-desc">Name: Z to A</option>
@@ -62,7 +96,7 @@ const Shop = () => {
             </select>
             <div className="row g-4">
 
-              {filteredData.length === 0 ? products.map((item) => (
+              {filteredData.length === 0 ? currentProducts.map((item) => (
                 <div className='col-12 col-sm-6 col-md-6 col-lg-4' key={item.id} data-aos="fade-up" data-aos-duration="2000">
                   <div className="card" >
                     <div className="product-image-con">
@@ -184,6 +218,11 @@ const Shop = () => {
                   </div>
 
                 </div>
+              ))}
+            </div>
+            <div className="pagination-btn-con" data-aos="fade-up" data-aos-duration="2000">
+              {pageNumbers.map((item,index)=>(
+                <button className={`pagination-button m-2 ${item===currentPage?"active":""}`} key={index} onClick={()=>{setCurrentPage(item)}}>{item}</button>
               ))}
             </div>
           </div>
--- Rostay Restaurant/src/components/home/SpecialMenu.jsx
+++ Rostay Restaurant/src/components/home/SpecialMenu.jsx
@@ -1,19 +1,24 @@
 import Aos from 'aos'
 import React from 'react'
 import { HiOutlineArrowLongRight } from 'react-icons/hi2'
+import { useSelector } from 'react-redux';
 
 const SpecialMenu = () => {
     Aos.init();
+    const categories = useSelector(p => p.category);
     return (
         <section className='special-menu-section container-fluid'>
             <h5 data-aos="zoom-in" data-aos-duration="1500">Our Special Menu</h5>
             <ul data-aos="zoom-in" data-aos-duration="1500" >
-                <li>
-                    <img className='img-1' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/gallery-10.jpg" alt="" />
-                    <p>Appetizers</p>
-                    <img className='img-2' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/h1-gallery1.jpg" alt="" />
-                </li>
-                <li>
+                {categories.map((item, index) => (
+                    <li>
+                        <img className='img-1' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/gallery-10.jpg" alt="" />
+                        <p>{item.categoryName}</p>
+                        <img className='img-2' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/h1-gallery1.jpg" alt="" />
+                    </li>
+                ))}
+
+                {/* <li>
                     <img className='img-1' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/category-menu-1.jpg" alt="" />
                     <p>Main dishes</p>
                     <img className='img-2' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/category-menu-2.jpg" alt="" />
@@ -32,7 +37,7 @@ const SpecialMenu = () => {
                     <img className='img-1' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/category-menu-3.jpg" alt="" />
                     <p>Cold Drinks</p>
                     <img className='img-2' src="https://demo2.themelexus.com/rostay/wp-content/uploads/2025/03/gallery-3.jpg" alt="" />
-                </li>
+                </li> */}
             </ul>
             <button data-aos="zoom-in" data-aos-duration="1500">View All Menu <HiOutlineArrowLongRight size={20} /></button>
 
